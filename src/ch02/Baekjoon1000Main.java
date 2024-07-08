1b85fe999366b66b06c0286b167a2d75891de47f
이승용 <iseung-yong@iseung-yong-ui-MacBookPro.local>
2024-07-09 04:57:13 +0900

    백준 1000번 문제풀이

---
+++ src/ch02/Baekjoon1000Main.java
@@ -0,0 +1,12 @@
+package ch02;
+
+import java.util.Scanner;
+
+public class Baekjoon1000Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+    }
+}
