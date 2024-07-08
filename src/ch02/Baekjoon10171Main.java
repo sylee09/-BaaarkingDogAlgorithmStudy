8c37471e4b7f49b21eaa4978d8392c94fb310ed5
이승용 <iseung-yong@iseung-yong-ui-MacBookPro.local>
2024-07-09 05:05:29 +0900

    백준 10171 문제 풀이

---
+++ src/ch02/Baekjoon10171Main.java
@@ -0,0 +1,11 @@
+package ch02;
+
+public class Baekjoon10171Main {
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
