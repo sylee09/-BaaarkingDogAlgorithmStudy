999a77aea7a0dd966cceb3361d3a3921b2c0333c
이승용 <iseung-yong@iseung-yong-ui-MacBookPro.local>
2024-07-09 13:29:14 +0900

    백준 2442번 문제 문제풀이

---
+++ src/ch02/Baekjoon2442Main.java
@@ -0,0 +1,19 @@
+package ch02;
+
+import java.util.Scanner;
+
+public class Baekjoon2442Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int space = 1; space <= n - i; space++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
